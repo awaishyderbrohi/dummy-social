3cd9f30dca8ab741e73ef655fedd46213804152f
Awais Hyder <awaishyderbr@gmail.com>
2025-06-21 13:07:31 +0500

    project created

---
+++ src/main/java/com/awaishyder/dummy_social/DummySocialApplication.java
@@ -0,0 +1,13 @@
+package com.awaishyder.dummy_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummySocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummySocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awaishyder/dummy_social/DummySocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awaishyder.dummy_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummySocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
